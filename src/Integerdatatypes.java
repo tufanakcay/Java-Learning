3936f8f5d2935f52967791b6bf1a9637233528b9
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-07-05 23:40:31 +0300

    Create Integerdatatypes.java

---
+++ src/Integerdatatypes.java
@@ -0,0 +1,15 @@
+
+public class Integerdatatypes {
+    public static void main(String[] args) {
+                // byte --> short --> int --> long
+        
+        short i = 100;
+        byte j = 2;
+        int k = 4;
+        
+        long d = i + j + k;
+        System.out.println(d);
+        
+        
+    }
+}
