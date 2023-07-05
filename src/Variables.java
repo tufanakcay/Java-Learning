310bcc9966fe5e3a6ca68d667ef6c5330822a4e3
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-07-05 23:16:20 +0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+
+public class Variables {
+    public static void main(String[] args) {
+        int num1 =10;
+        int num2 =15;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("Sum : " + sum);
+    }
+}
