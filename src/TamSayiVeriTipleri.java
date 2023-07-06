904c06e4f7c0cc1348a0cb65fcfe77b6a5a5d53a
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-07-06 23:19:37 +0300

    main

---
+++ src/TamSayiVeriTipleri.java
@@ -0,0 +1,46 @@
+
+public class TamSayiVeriTipleri {
+    
+    public static void main(String[] args) {
+        
+        int a = 40;
+        System.out.println("Integer");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.BYTES);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.TYPE);
+        System.out.println("----------------------");
+        
+        System.out.println("Byte");
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.BYTES);
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.TYPE);
+        System.out.println("----------------------");
+        
+        System.out.println("Short");
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.BYTES);
+        System.out.println(Short.SIZE);
+        System.out.println(Short.TYPE);
+        System.out.println("----------------------");
+        
+        System.out.println("Long");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.BYTES);
+        System.out.println(Long.SIZE);
+        System.out.println(Long.TYPE);
+        System.out.println("----------------------");
+        
+        
+        byte b = 100;
+        byte c = (byte)(b/2);
+        System.out.println(c);
+        
+        
+    }
+}
