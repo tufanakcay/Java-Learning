57f1da7c04e075dc67d8f807a19da726cb60ba0d
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-07-07 22:58:09 +0300

    main

---
+++ src/DataInput.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class DataInput {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Age : ");
+        
+        int age = scanner.nextInt();
+        
+        System.out.println("Age : " + age);
+        
+    }
+}
